17c4812229bc6464432cfed6dce0e1a10c49ec50
passivlingo <info@passivlingo.com>
2021-06-06 10:00:14 +0200

    add versioning to lib install

--- PostInstall/post_setup_dictionary.py
+++ PostInstall/post_setup_dictionary.py
@@ -1,4 +1,4 @@
 import os
 
 print('Installing Python Libraries for Passivlingo Multilingual Dictionary ...')    
-os.system('pip3 install passivlingo-dictionary')    
+os.system('pip3 install passivlingo-dictionary==0.0.2')    
